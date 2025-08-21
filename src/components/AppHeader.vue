45170808ed0ebbeb832a8d5433da6f581d6ef9c8
another <schibkoartem@yandex.ru>
2025-08-21 17:23:08 +0400

    База + настройка + удаления ненужной инфо + создания Header + импорт стилей с primevue и их исп для Header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,47 @@
+<template>
+   <app-menubar :model="items" class="menu">
+    <template #item="{ item, props }">
+        <router-link :to="item.path" class="flex align-items-center"
+            v-bind="props.action">
+            <span :class="item.icon" class="p-menuitem-icon"></span>
+            <span class="ml-2">{{ item.label }}</span>
+        </router-link>
+    </template>
+   </app-menubar>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+
+const items = ([
+    {
+        label: 'Авторизация',
+        icon: 'pi pi-user',
+        path: '/auth'
+    },
+    {
+        label: 'Добавить',
+        icon: 'pi pi-plus',
+        path: '/'
+    },
+    {
+        label: 'Список собеседований',
+        icon: 'pi pi-list',
+        path: '/list'
+    },
+    {
+        label: 'Статистика',
+        icon: 'pi pi-chart-pie',
+        path: '/statistic'
+    },
+])
+</script>
+
+<style scoped>
+.menu {
+  margin: 30px 0;
+}
+.menu-exit {
+  cursor: pointer;
+}
+</style>
