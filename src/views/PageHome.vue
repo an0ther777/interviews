0ee5195abcc21067a826554c42c9c27a7d1548d6
another <schibkoartem@yandex.ru>
2025-09-03 20:59:24 +0400

    Осталось отобразить данные, получаю в консоль + сортировка по дате

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -38,9 +38,10 @@ import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
 import type { IInterview } from '@/interfaces';
 import { v4 as uuidv4 } from "uuid"
-import { getAuth } from 'firebase/auth';
+import { useUserStore } from '@/stores/user';
 import { getFirestore, setDoc, doc } from 'firebase/firestore';
 
+const userStore = useUserStore()
 const db = getFirestore()
 const router = useRouter()
 
@@ -67,10 +68,9 @@ const addNewInterview = async (): Promise<void> => {
     createAr: new Date()
   }
 
-  const userId = getAuth().currentUser?.uid
-  if(userId) {
-    await setDoc(doc(db, `users/${userId}/interviews`, payload.id)
-      , payload).then(() => {
+  if(userStore.userId) {
+    await setDoc(doc(db, `users/${userStore.userId}/interviews`, payload.id)
+      ,payload).then(() => {
           router.push('/list')
       })
 
--- src/views/PageList.vue
+++ src/views/PageList.vue
@@ -1,11 +1,40 @@
 <template>
-    <div>
-List
-    </div>
+    <h1>Список собеседований</h1>
+    
 </template>
 
 <script setup lang="ts">
+import { getFirestore, collection, query,
+          orderBy, getDocs, deleteDoc, doc } from 'firebase/firestore';
+import { ref, onMounted } from 'vue';
+import { useUserStore } from '@/stores/user';
+import type { IInterview } from '@/interfaces';
 
+const userStore = useUserStore()
+const db = getFirestore()
+
+const interviews = ref<IInterview[]>([])
+const isLoading = ref<boolean>(true)
+
+const getAllInterviews = async <T extends IInterview>()
+    : Promise<T[]> => {
+
+  const getData = query(
+    collection(db, `users/${userStore.userId}/interviews`),
+    orderBy('createAr', "desc")
+  )
+
+  const listDocs = await getDocs(getData)
+
+  return listDocs.docs.map((doc) => doc.data() as T)
+}
+
+onMounted(async () => {
+  const listIntervies: Array<IInterview> = await getAllInterviews()
+    console.log(listIntervies);
+
+  interviews.value = [...listIntervies]
+})
 </script>
 
 <style lang="scss" scoped>
