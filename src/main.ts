b77ea51023e432bdf93b2a1e0fa8e6b731ebfbea
another <schibkoartem@yandex.ru>
2025-09-07 20:58:41 +0400

    Добавил изменеиние собесов и стиль

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,10 @@ import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
 import ConfirmDialog from 'primevue/confirmdialog'
 import InlineMessage from 'primevue/inlinemessage'
+import InputNumber from 'primevue/inputnumber'
+import Textarea from 'primevue/textarea'
+import Calendar from 'primevue/calendar'
+import Radio from 'primevue/radiobutton'
 
 import App from './App.vue'
 import router from './router'
@@ -52,5 +56,9 @@ app.component('app-datatable', DataTable)
 app.component('app-column', Column)
 app.component('app-dialog', ConfirmDialog)
 app.component('app-message', InlineMessage)
+app.component('app-input-number', InputNumber)
+app.component('app-textarea', Textarea)
+app.component('app-calendar', Calendar)
+app.component('app-radio', Radio)
 
 app.mount('#app')
--- src/views/PageInterview.vue
+++ src/views/PageInterview.vue
@@ -1,13 +1,188 @@
 <template>
-    <div>
-Interview
-    </div>
+<app-progress v-if="isLoading" />
+  <div class="content-interview" v-else-if="interview?.id && !isLoading">
+    <app-card>
+      <template #title>Собеседование в компанию {{ interview.company }}</template>
+      <template #content>
+        <div class="flex flex-column gap-2">
+          <label for="company">Компания</label>
+          <app-input-text class="input mb-3" id="company" v-model="interview.company" />
+        </div>
+        <div class="flex flex-column gap-2">
+          <label for="vacancyLink">Описание вакансии (ссылка)</label>
+          <app-input-text class="input mb-3" id="vacancyLink" v-model="interview.vacancyLink" />
+        </div>
+        <div class="flex flex-column gap-2">
+          <label for="hrName">Контакт (имя)</label>
+          <app-input-text class="input mb-3" id="hrName" v-model="interview.hrName" />
+        </div>
+        <div class="flex flex-column gap-2">
+          <label for="contactTelegram">Telegram username HR</label>
+          <app-input-text
+            class="input mb-3"
+            id="contactTelegram"
+            v-model="interview.contactTelegram"
+          />
+        </div>
+        <div class="flex flex-column gap-2">
+          <label for="contactWhatsApp">WhatsApp HR</label>
+          <app-input-text
+            class="input mb-3"
+            id="contactWhatsApp"
+            v-model="interview.contactWhatsApp"
+          />
+        </div>
+        <div class="flex flex-column gap-2">
+          <label for="contactPhone">Телефон HR</label>
+          <app-input-text class="input mb-3" id="contactPhone" v-model="interview.contactPhone" />
+        </div>
+        <div class="flex flex-wrap gap-3 p-fluid mb-3">
+          <div class="flex-auto">
+            <app-input-number style="padding: 4px 10px; border: 1px solid grey; border-radius: 5px;"
+              inputId="salaryFrom"
+              placeholder="Зарплатная вилка от"
+              v-model="interview.salaryFrom"
+            />
+          </div>
+          <div class="flex-auto">
+            <app-input-number style="padding: 4px 10px; border: 1px solid grey; border-radius: 5px;"
+              inputId="salaryTo"
+              placeholder="Зарплатная вилка до"
+              v-model="interview.salaryTo"
+            />
+          </div>
+        </div>
+
+        <app-button style="border-radius: 5px; padding: 5px 10px;"
+          label="Добавить этап"
+          severity="info"
+          icon="pi pi-plus"
+          class="mb-3"
+          @click="addStage"
+        />
+        <template v-if="interview.stages">
+          <div v-for="(stage, index) in interview.stages" :key="index" class="interview-stage">
+            <div class="flex flex-column gap-2">
+              <label :for="`stage-name-${index}`">Название этапа</label>
+              <app-input-text class="input mb-3" :id="`stage-name-${index}`" v-model="stage.name" />
+            </div>
+            <div class="flex flex-column gap-2">
+              <label :for="`stage-date-${index}`">Дата прохождения этапа</label>
+              <input
+              type="number"
+                class="input mb-3"
+                :id="`stage-date-${index}`"
+                v-model="stage.date"
+              />
+            </div>
+            <div class="flex flex-column gap-2">
+              <label :for="`stage-description-${index}`">Комментарий</label>
+              <app-textarea
+                :id="`stage-description-${index}`"
+                class="input mb-3"
+                rows="5"
+                v-model="stage.description"
+              />
+            </div>
+            <app-button style="border-radius: 5px;
+            padding: 5px 10px;" severity="danger" label="Удалить этап" @click="removeStage" />
+          </div>
+        </template>
+
+        <div class="flex flex-wrap gap-3 mb-3">
+          <div class="flex align-items-center">
+            <app-radio
+              inputId="interviewResult1"
+              name="result"
+              value="Refusal"
+              v-model="interview.result"
+            />
+            <label for="interviewResult1" class="ml-2" style="margin-right: 10px;">Отказ</label>
+          </div>
+          <div class="flex align-items-center">
+            <app-radio
+              inputId="interviewResult2"
+              name="result"
+              value="Offer"
+              v-model="interview.result"
+            />
+            <label for="interviewResult2" class="ml-2">Оффер</label>
+          </div>
+        </div>
+
+        <app-button class="mb-3" label="Сохранить" style="border-radius: 5px;
+            padding: 5px 10px; background-color: rgb(51, 143, 230); color: white;"
+          icon="pi pi-save" @click="saveInterview" />
+      </template>
+    </app-card>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { getFirestore, doc, getDoc, updateDoc } from 'firebase/firestore'
+import { useUserStore } from '@/stores/user'
+import type { IInterview, IStage } from '@/interfaces'
 
+
+const db = getFirestore()
+const userStore = useUserStore()
+const route = useRoute()
+
+const isLoading = ref<boolean>(true)
+const interview = ref<IInterview>()
+
+const docref = doc(db, `users/${userStore.userId}/interviews`, route.params.id as string)
+
+const getData = async (): Promise<void> => {
+  isLoading.value = true
+  const docSnap = await getDoc(docref)
+  interview.value = docSnap.data() as IInterview
+  isLoading.value = false
+  console.log(interview.value)
+}
+
+const saveInterview = async (): Promise<void> => {
+  isLoading.value = true
+  await updateDoc(docref, { ...interview.value })
+  await getData()
+}
+
+const addStage = () => {
+  if (interview.value) {
+    if (!interview.value.stages) {
+      interview.value.stages = []
+    }
+    interview.value.stages.push({ name: '', date: '', description: '' })
+  }
+}
+
+const removeStage = (index: number) => {
+  if (interview.value) {
+    if (interview.value.stages) {
+      interview.value.stages.splice(index, 1)
+    }
+  }
+}
+
+onMounted(async () => await getData())
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.content-interview {
+  max-width: 600px;
+  margin: auto;
+}
+.input {
+  width: 100%;
+  border: 1px solid gray;
+  border-radius: 5px;
+}
+.interview-stage {
+  border: 1px solid #ccc;
+  border-radius: 6px;
+  padding: 10px;
+  margin-bottom: 10px;
+}
 </style>
